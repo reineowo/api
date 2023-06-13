4a37eae30a1764a48e04420993eb4ce6efb64c10
Lê Đức Thuận <87764627+YuukiCute@users.noreply.github.com>
2023-06-13 19:24:21 +0700

    Update app.js

--- rushia/app.js
+++ rushia/app.js
@@ -1,6 +1,9 @@
+/*
 function randomtext() {
     //var text = `https://raw.githubusercontent.com/YuukiCute/data/main/rushia/${Math.floor(Math.random()*866)}.jpg`;
-    var num = 866;
-    return num;
+    return text;
 }
 document.getElementById('a').innerHTML = randomtext()
+*/
+
+866
